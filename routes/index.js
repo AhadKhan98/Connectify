c1db7f569ed1c21c16a84fe0fc52689dd4a802ba
vrushti-mody <vrushtimody6@gmail.com>
2020-10-07 19:48:41 +0530

    Integrated Firebase to the application

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
-
+const database = require('../models/firebase');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/signUp', function(req, res, next) {
+  const username=req.body.username;
+  const email=req.body.email;
+  const password = req.body.password;
+  const subjects = req.body.subjects;
+  const online = req.body.online;
+  const user={username,email,password,subjects,online}
+  database.ref('users').push(user).then((ref)=>{
+    res.send(200)
+  }).catch((err)=>{
+    res.send(err)
+  })
 });
 
 module.exports = router;
