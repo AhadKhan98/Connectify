dd536a25bf1c644c03c766d34371fe0fdc806a66
AhadKhan98 <ahadkhan98@hotmail.com>
2020-10-11 13:03:42 +0500

    create firestore document only if new user

--- models/firestore.js
+++ models/firestore.js
@@ -2,13 +2,22 @@ const firebaseModel = require("../models/firebase");
 
 // ADD NEW USER WITH DEFAULT PARAMS
 const addNewUserToDatabase = ({db, result}) => {
-    console.log("BEFORE INSERTING", );
-    data = {
-        displayName: result.displayName,
-        email: result.email,
-        completedProfile: false,
-    };
-    db.collection("users").doc(result.email).set({data});
+    // Check if user already exists in firestore
+    db.collection("users").doc(result.email).get().then((document) => {
+      if (document.exists) {
+        console.log("USER EXISTS");
+      } else {
+        console.log("CREATING NEW USER", result);
+        data = {
+          displayName: result.displayName,
+          email: result.email,
+          completedProfile: false,
+      };
+      db.collection("users").doc(result.email).set({data});
+      }
+    });
+
+    
   };
 
 module.exports = {addNewUserToDatabase};
