3c2a68bb05dd6bf3286aae802bb74c87d888afd1
AhadKhan98 <ahadkhan98@hotmail.com>
2020-10-13 15:21:28 +0500

    add authentication to routes

--- routes/index.js
+++ routes/index.js
@@ -29,7 +29,12 @@ router.get("/", function (req, res, next) {
 
 // Show Sign Up Page
 router.get("/signUp", function (req, res, next) {
-  res.render("signup", {errorMessage:""});
+  if (currentUser) {
+    res.redirect('/');
+  } else {
+    res.render("signup", {errorMessage:""});
+  }
+  
 });
 
 // Form Submit (POST METHOD)
@@ -99,7 +104,12 @@ router.get("/googlesignintoken", function(req,res,next) {
 
 // Show Login Page
 router.get("/login", function (req, res, next) {
-  res.render("login", {errorMessage:""});
+  if (currentUser) {
+    res.redirect('/');
+  } else {
+    res.render("login", {errorMessage:""});
+  }
+  
 });
 
 // Form Submit (POST METHOD)
@@ -142,7 +152,12 @@ router.get("/checkUserProfile", function (req, res, next) {
 });
 
 router.get("/complete-profile", function (req,res,next) {
-  res.render("complete-profile", {displayName:currentUser.displayName})
+  if (currentUser) {
+    res.render("complete-profile", {displayName:currentUser.displayName});
+  } else {
+    res.redirect('/');
+  }
+  
 });
 
 router.post("/complete-profile/submit", function(req,res,next) {
@@ -155,10 +170,13 @@ router.post("/complete-profile/submit", function(req,res,next) {
 
 /* STUDY ROOM FUNCTIONALITY */
 router.get('/chat', function(req, res, next) {
-  firestore.getUserPoints({db:firebaseModel.db, user:currentUser}).then(result => {
+  if (currentUser) {
+    firestore.getUserPoints({db:firebaseModel.db, user:currentUser}).then(result => {
     res.render('chat', {username:currentUser.displayName, userPoints:result.points});
-  })
-  
+  });
+  } else {
+    res.redirect('/');
+  }
 });
 
 
