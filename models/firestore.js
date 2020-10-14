f792229807e1003b419433b75bd5e8a540d97527
AhadKhan98 <ahadkhan98@hotmail.com>
2020-10-14 15:11:25 +0500

    render data from firebase to profile.ejs

--- models/firestore.js
+++ models/firestore.js
@@ -71,7 +71,13 @@ const addPostToDatabase = ({db, email, postContent, postType, timestamp}) => {
     }).catch(error => {
       return error
     })
+};
 
+// GET USER'S POSTS
+const getUserPosts = ({db, user}) => {
+  return db.collection("users").doc(user.email).get().then((snapshot) => {
+    return snapshot.data().posts;
+  });
 };
 
 // GET USER'S POINTS
@@ -121,5 +127,6 @@ module.exports = {
   getUserPoints,
   addUserPoints,
   updateUserProfile,
-  addPostToDatabase
+  addPostToDatabase,
+  getUserPosts,
 };
